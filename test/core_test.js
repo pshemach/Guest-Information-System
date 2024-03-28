a1b0b24d63836745e46b081bedcd58f2a9635f9e
Pasindu <pasindusandakan@gmail.com>
2024-03-28 18:29:52 +0530

    test files added

---
+++ test/core_test.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+// fs.writeFileSync("data.txt", "Hellow");
+// fs.appendFileSync("data.txt", " World");
+
+// const data = fs.readFileSync("data.txt");
+// console.log(data.toString());
